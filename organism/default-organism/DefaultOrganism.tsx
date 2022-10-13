579db7101cb2679883878469f33731c1e5a2a2c2
Nicolas_SuRo <nicolas.suarez@nivelics.co>
2022-10-13 12:07:16 -0500

    config styledComponents

---
+++ organism/default-organism/DefaultOrganism.tsx
@@ -0,0 +1,11 @@
+import { StyledParagraph } from "./Default.style";
+
+const Default = ({children}:any) => {
+    return (
+        <StyledParagraph>
+            {children}
+        </StyledParagraph>
+    );
+}
+
+export default Default;
